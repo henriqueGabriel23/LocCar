b645a677e3ea274b9fdaef48c8ee815edd4aa401
henrique gabriel <hg98030285@gmail.com>
2022-11-24 18:39:29 -0300

    Visual carros finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HomeComponent } from './home/home.component';
+import { CarrosComponent } from './carros/carros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CarrosComponent
   ],
   imports: [
     BrowserModule,
