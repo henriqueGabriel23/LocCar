117364abcbe814fc8036c1f44719a9bcf38fcaaf
henrique gabriel <hg98030285@gmail.com>
2022-11-25 16:10:08 -0300

    pull brunches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,25 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { HomeComponent } from './home/home.component';
 import { CarrosComponent } from './carros/carros.component';
 import { PerfiladmComponent } from './perfiladm/perfiladm.component';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CadastroComponent,
-    HomeComponent,
-    CarrosComponent,
-    PerfiladmComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CadastroComponent,
+        HomeComponent,
+        CarrosComponent,
+        PerfiladmComponent,
+        HeaderComponent,
+        FooterComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        
+    ]
 })
 export class AppModule { }
