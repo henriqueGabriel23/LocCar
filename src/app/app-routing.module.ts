b60b58211e50a0c34fb95ec9c198d4d67541cfeb
henrique gabriel <hg98030285@gmail.com>
2022-11-27 11:21:26 -0300

    rotas feita

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { CarrosComponent } from './carros/carros.component';
+import { HomeComponent } from './home/home.component';
+import { PerfiladmComponent } from './perfiladm/perfiladm.component';
 
-const routes: Routes = [];
+const routes: Routes = [
 
+  {path:'', redirectTo:'home',pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'cadastro', component:CadastroComponent},
+  {path:'carros', component:CarrosComponent},
+  {path:'perfil', component:PerfiladmComponent}
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
---
+++ src/app/model/models.model.ts
@@ -0,0 +1,6 @@
+export interface Cadastro{
+    id:number,
+    nomeCarro:string,
+    portas:number,
+    numPessoas:number
+}
