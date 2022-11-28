de446ef582d1205500022a0ea9d542015c76d561
Gabrielly Cardoso <gabriellybrancocardoso@gmail.com>
2022-11-28 18:28:33 -0300

    perfil-cliente finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { PerfilClienteComponent } from './perfil-cliente/perfil-cliente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroComponent,
+    PerfilClienteComponent,
   ],
   imports: [
     BrowserModule,
