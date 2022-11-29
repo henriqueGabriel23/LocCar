e95ca44451862986a7712cd268855221260731f5
Tiphany De Oliveira Marques <tiphanymarques@brq.com>
2022-11-29 16:41:31 -0300

    reserva em andamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ReservaComponent } from './reserva/reserva.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ReservaComponent
   ],
   imports: [
     BrowserModule,
